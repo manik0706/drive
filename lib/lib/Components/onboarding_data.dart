d4fab0a7dfa964b5961acd88cc825140d089c1f9
Manik <72183005+manik0706@users.noreply.github.com>
2024-01-18 11:47:47 +0530

    Add files via upload

---
+++ lib/lib/Components/onboarding_data.dart
@@ -0,0 +1,18 @@
+import 'onboardingInfo.dart';
+
+class OnboardingData {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        title: "lets ride",
+        description: "High qualities rides",
+        image: "assets/travel.png"),
+    OnboardingInfo(
+        title: "Interesting stories",
+        description: "high qualities ride",
+        image: "assets/travel.png"),
+    OnboardingInfo(
+        title: "on time",
+        description: "always on time",
+        image: "assets/travel.png"),
+  ];
+}
